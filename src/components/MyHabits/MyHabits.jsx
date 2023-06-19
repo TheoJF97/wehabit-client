b114fec6852e4ab98c1c80e6037561aa40f6a81e
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-19 16:03:00 -0400

    Align checkboxes and habits main component

--- src/components/MyHabits/MyHabits.jsx
+++ src/components/MyHabits/MyHabits.jsx
@@ -2,19 +2,29 @@ export default function MyHabits() {
   return (
     <>
       <section className="myhabits">
-        <h1 className="myhabits__title">MyHabits</h1>
-        {/* <div className="myhabits__check-container">
-          <label htmlFor="check">Checkbox</label>
-          <input type="checkbox" id="check" className="myhabits__check" />
-        </div> */}
-        <div className="wrapper">
-          <label>Drink Water</label>
-          <input type="checkbox" id="check" className="myhabits__check" />
-          <input type="checkbox" id="check" className="myhabits__check" />
-          <input type="checkbox" id="check" className="myhabits__check" />
-          <input type="checkbox" id="check" className="myhabits__check" />
-          <input type="checkbox" id="check" className="myhabits__check" />
-          <input type="checkbox" id="check" className="myhabits__check" />
+        <h1 className="myhabits__header">MyHabits - [username]</h1>
+        <div className="myhabits__container">
+          <div className="myhabits__dates">
+            <h2 className="myhabits__title myhabits__title--empty">[habit1]</h2>
+            <span className="myhabits__date">Sun, Jun 25</span>
+            <span className="myhabits__date">Mon, Jun 26</span>
+            <span className="myhabits__date">Tue, Jun 27</span>
+            <span className="myhabits__date">Wed, Jun 28</span>
+            <span className="myhabits__date">Thu, Jun 29</span>
+            <span className="myhabits__date">Fri, Jun 30</span>
+            <span className="myhabits__date">Sat, Jul 1</span>
+          </div>
+
+          <div className="myhabits__habit">
+            <h2 className="myhabits__title">[habit1]</h2>
+            <input type="checkbox" id="check" className="myhabits__check" />
+            <input type="checkbox" id="check" className="myhabits__check" />
+            <input type="checkbox" id="check" className="myhabits__check" />
+            <input type="checkbox" id="check" className="myhabits__check" />
+            <input type="checkbox" id="check" className="myhabits__check" />
+            <input type="checkbox" id="check" className="myhabits__check" />
+            <input type="checkbox" id="check" className="myhabits__check" />
+          </div>
         </div>
       </section>
     </>
