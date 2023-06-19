4812782aeb1f8311be84591d33995f77f9498a86
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-19 11:43:22 -0400

    Create myhabits component boiilerplate

---
+++ src/components/MyHabits/MyHabits.jsx
@@ -0,0 +1,7 @@
+export default function MyHabits() {
+  return (
+    <>
+      <h1 className="myhabits__title">MyHabits</h1>
+    </>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ export default function HomePage() {
   return (
     <>
       <Header />
+      
     </>
   );
 }
