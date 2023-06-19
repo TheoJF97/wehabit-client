15b7a2a221159ea1438ef09a903a020049a9409f
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-19 12:48:26 -0400

    input checkbox styling

--- src/components/MyHabits/MyHabits.jsx
+++ src/components/MyHabits/MyHabits.jsx
@@ -1,7 +1,22 @@
 export default function MyHabits() {
   return (
     <>
-      <h1 className="myhabits__title">MyHabits</h1>
+      <section className="myhabits">
+        <h1 className="myhabits__title">MyHabits</h1>
+        {/* <div className="myhabits__check-container">
+          <label htmlFor="check">Checkbox</label>
+          <input type="checkbox" id="check" className="myhabits__check" />
+        </div> */}
+        <div className="wrapper">
+          <label>Drink Water</label>
+          <input type="checkbox" id="check" className="myhabits__check" />
+          <input type="checkbox" id="check" className="myhabits__check" />
+          <input type="checkbox" id="check" className="myhabits__check" />
+          <input type="checkbox" id="check" className="myhabits__check" />
+          <input type="checkbox" id="check" className="myhabits__check" />
+          <input type="checkbox" id="check" className="myhabits__check" />
+        </div>
+      </section>
     </>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
+// Import components
 import Header from "../../components/Header/Header";
+import MyHabits from "../../components/MyHabits/MyHabits";
 
 export default function HomePage() {
   return (
     <>
       <Header />
-      
+      <MyHabits />
     </>
   );
 }
