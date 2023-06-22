fdfaddb28662f20cbadf145092edc261e2242fd0
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-22 07:30:37 -0400

    Get completions for habit by id

--- src/components/Habit/Habit.jsx
+++ src/components/Habit/Habit.jsx
@@ -2,42 +2,35 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
-export default function Habit({ habit, habitId }) {
+export default function Habit({ habit }) {
   //Deconstruct prop habit (id, name, email) for name:
-  const { title, user_id } = habit;
+  const { title, id } = habit;
 
   //State variables
-  const [completion, setCompletion] = useState([]);
+  const [completions, setCompletions] = useState([]);
   const [hasError, setHasError] = useState(false);
 
   //Import server url from .env
   const { REACT_APP_SERVER_URL: serverUrl } = process.env;
 
-  // Grab id
-  let { id } = useParams();
-
-  //handle on change function
-  // const handleOnChange = (event) => {
-  //   event.preventdefault()
-  // };
-
   //Axios call to get a user's habits
-  // useEffect(() => {
-  //   axios
-  //     .get(`${serverUrl}/users/${id}/habits`)
-  //     .then((response) => {
-  //       setHabits(response.data);
-  //       console.log(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //       setHasError(true);
-  //     });
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(`${serverUrl}/habits/${id}/completions`)
+      .then((response) => {
+        setCompletions(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setHasError(true);
+      });
+  }, []);
 
-  // if (!completion) {
-  //   return <span>Loading.....</span>;
-  // }
+  if (!completions) {
+    return <span>Loading.....</span>;
+  }
+
+  console.log(`${id} ${completions}`);
 
   if (hasError) {
     return <h1>Information not found</h1>;
@@ -45,13 +38,13 @@ export default function Habit({ habit, habitId }) {
 
   return (
     <>
-      <div key={habitId} className="habit__habit">
+      <div key={id} className="habit__habit">
         <h2 className="habit__title">{title}</h2>
         <input
           type="checkbox"
           id="check"
           className="habit__check"
-          onChange={(e) => setCompletion(e.target.value)}
+          onChange={(e) => setCompletions(e.target.value)}
         />
         <input type="checkbox" id="check" className="habit__check" />
         <input type="checkbox" id="check" className="habit__check" />
